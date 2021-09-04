70a05148d6f12ef5df9f59026c3cf9ecdccf7d7d
Heosve <juanjosehenao1810@gmail.com>
2021-09-03 21:33:59 -0500

    commit

--- controller/logic/teacher.controller.js
+++ controller/logic/teacher.controller.js
@@ -75,41 +75,56 @@ exports.updateTeacher = (req, res, next) =>{
             );
         }
         console.log("Fuera:")
-        if(req.body.olddocument !=undefined){
-            let r = config.get("roles").teacher;
-            console.log("Old Document: "+req.body.olddocument)
-            let user = {
-                name: teacher.name,
-                lastname: teacher.lastname,
-                username: teacher.document,
-                password: helper.EncryptPassword(req.body.password),
-                rol: r
-            }
-            console.log("Hola")
-            userDto.updateOne({username:req.body.olddocument},user, (err, u) => {
-                if(err){
-                    return res.status(400).json(
-                        {
-                            error: err
-                        }
-                    );
-                }
-                console.log("Buenas")
-                notHelper.sendSMS(teacher.phone);
-                return res.status(201).json(
+
+    });
+    if(req.body.olddocument !=undefined){
+        let r = config.get("roles").teacher;
+        console.log("Old Document: "+req.body.olddocument)
+        let user = {
+            name: teacher.name,
+            lastname: teacher.lastname,
+            username: teacher.document,
+            password: helper.EncryptPassword(req.body.password),
+            rol: r
+        }
+        userDto.update({username:req.body.olddocument},user, (err, u) => {
+            try{
+    
+        
+            if(err){
+                return res.status(400).json(
                     {
-                        info: data
+                        error: err
                     }
                 );
-            });
-        }else{
-            res.status(201).json(
-            {
-                info: data
             }
-        );
+            console.log("Buenas")
+            notHelper.sendSMS(teacher.phone);
+            return res.status(201).json(
+                {
+                    info: data
+                }
+            );
+            }catch(error){
+                console.log(error)
+            }
+        });
+        console.log("Hola")
+        try{
+        
+    
+    }catch(error){
+        console.log("Hola como estas")
+        console.log(error)
+    }
+    }else{
+        res.status(201).json(
+        {
+            info: data
         }
-    });
+    );
+    }
+
 };
 
 exports.getAll = (req, res, next) =>{
